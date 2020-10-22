7f08bb36941f5ef906b97dc5c69fe88283712e07
Ivan Kuzmin <snobbaspirit@gmail.com>
2020-10-22 03:04:16 +0300

    added materializecss, added partials, installed express-handlebars

--- index.js
+++ index.js
@@ -5,25 +5,30 @@ const path = require('path');
 
 const app = express();
 
-const handlebars = exphbs.create({
+const hbs = exphbs.create({
     defaultLayout: 'main',
-    extname: 'handlebars',
+    extname: 'hbs',
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+app.get('/', (req, res, next) => {
+    res.render('index');
+    // res.sendFile(path.join(__dirname, 'views', 'index.html'))
 });
 
 
-//чтобы зарегистрировать движок 
-
-app.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
+app.use(express.static('public'));
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'about.html'))
-})
+    res.render('about');
+    // res.sendFile(path.join(__dirname, 'views', 'about.html'))
+});
 
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}...`)
-})
+    console.log(`Server is running on port ${PORT}...`);
+});
